d75550fa844868b05f74c2dd45ded9f5056febdb
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-13 23:57:03 +0300

    Added post question route

--- run.py
+++ run.py
@@ -10,7 +10,24 @@ def welcome():
 
 @app.route("/questions", methods=['GET'])
 def get_all_questions():
-    return jsonify({"Questions" : Questions}), 200
+    return jsonify({"Status":"Ok", "Questions" : Questions}), 200
+
+@app.route("/questions", methods=['POST'])
+def post_question():
+    data =request.get_json()
+    id = len(Questions) + 1
+    title = data['title']
+    description = data['description']
+    body = data['body']
+    Question = {
+        "id": id,
+        "title": title,
+        "description": description,
+        "body": body
+    }
+
+    Questions.append(Question)
+    return jsonify({"Status": "Ok", 'Message': "The Question was added successfully"}), 201
 
 if __name__ == "__main__":
     app.run(debug = True)
