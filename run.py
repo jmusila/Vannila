7e26e64acae94178adeeaa9307586cc9a930f1c3
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-13 18:58:28 +0300

    Added the main route

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return jsonify({"Hello World!"})
+def welcome():
+    return "Welcome to StackOverflow-Lite!"
+
 
 
 if __name__ == "__main__":
