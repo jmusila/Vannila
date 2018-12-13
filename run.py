f099a4d27ae6663c2b5802ccc46d0a99c4ecd5c6
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-14 00:24:49 +0300

    Added the get question by id route

--- run.py
+++ run.py
@@ -29,5 +29,15 @@ def post_question():
     Questions.append(Question)
     return jsonify({"Status": "Ok", 'Message': "The Question was added successfully"}), 201
 
+
+@app.route("/questions/<int:id>", methods=['GET'])
+def get_question_by_id(id):
+    for question in Questions:
+        if question['id'] == id:
+            return jsonify({"Status": "Ok", "Question": question}), 200
+        else:
+            return jsonify({"Message" : "Question with that id not found", "Status" : "Error"}), 404
+    
+
 if __name__ == "__main__":
     app.run(debug = True)
