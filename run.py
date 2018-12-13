ba6e800af72fba6fc75b8f956b529d839684d22e
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-13 19:07:49 +0300

    Added the second route to get all questions

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+Questions = []
 
 @app.route("/")
 def welcome():
     return "Welcome to StackOverflow-Lite!"
 
-
+@app.route("/questions", methods=['GET'])
+def get_all_questions():
+    return jsonify({"Questions" : Questions}), 200
 
 if __name__ == "__main__":
     app.run(debug = True)
