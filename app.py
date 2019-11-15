75cb3195a0aeac734aa315c788e994afdbb05251
jonathanmusila <jonathanmusila6@gmail.com>
2019-11-15 07:32:30 +0300

    configured the app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 Questions = []
 
@@ -60,4 +61,4 @@ def delete_question(id):
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
