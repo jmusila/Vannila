53c1337fc2504a9db57436bf5d3a1db345ce445e
jonathanmusila <jonathanmusila6@gmail.com>
2019-11-15 22:07:36 +0300

    added config classes

--- config.py
+++ config.py
@@ -1,9 +1,34 @@
 import os
 
 
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
+class Config(object):
+    SECRET_KEY = os.urandom(32)
+    # Grabs the folder where the script runs.
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
+    # Enable debug mode.
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    """ Development configurations """
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """ Production configurations """
+    DEBUG = True
+
+class TestingConfig(Config):
+    """ Testing configurations """
+    TESTING = True
+    DEBUG = True
+
+class StagingConfig(Config):
+    """ Staging configurations """
+    DEBUG = True
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig
+}
