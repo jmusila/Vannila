1212d6e8c638c65f7841e6ec29f3454937567036
jonathanmusila <jonathanmusila6@gmail.com>
2019-11-19 14:23:25 +0300

    styled the tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,12 +26,14 @@ class TestQuestions(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
 
     def test_post_question(self):
-        res = self.client.post("/questions", content_type='application/json', data=json.dumps(self.question))
+        res = self.client.post(
+            "/questions", content_type='application/json', data=json.dumps(self.question))
         self.assertEqual(res.status_code, 201)
         self.assertIn('The Question was added successfully', str(res.data))
 
     def test_get_single_question(self):
-        self.client.post("/questions", content_type='application/json', data=json.dumps(self.question))
+        self.client.post(
+            "/questions", content_type='application/json', data=json.dumps(self.question))
         response = self.client.get("/questions/1")
         self.assertEqual(response.status_code, 200)
 
@@ -41,17 +43,18 @@ class TestQuestions(unittest.TestCase):
         self.assertIn('Question with that id not found', str(res.data))
 
     def test_update_question(self):
-        self.client.post("/questions", content_type='application/json', data=json.dumps(self.question))
+        self.client.post(
+            "/questions", content_type='application/json', data=json.dumps(self.question))
         response = self.client.put("questions/1")
         self.assertEqual(response.status_code, 201)
         self.assertIn('Question updated successfully', str(response.data))
 
-
     def test_delete_question(self):
-        self.client.post("/questions", content_type='application/json', data=json.dumps(self.question))
+        self.client.post(
+            "/questions", content_type='application/json', data=json.dumps(self.question))
         response = self.client.delete("questions/1")
         self.assertEqual(response.status_code, 204)
-    
+
     def test_put_no_question(self):
         response = self.client.put("questions/1")
         self.assertEqual(response.status_code, 404)
@@ -59,7 +62,6 @@ class TestQuestions(unittest.TestCase):
     def test_delete_no_question(self):
         response = self.client.delete("questions/1")
         self.assertEqual(response.status_code, 404)
-
 
     def tearDown(self):
         return super().tearDown()
